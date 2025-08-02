9a200a1032435efee1c6dd4dad4b72b1f1b737a3
rajmandal800 <rajmandal800@gmail.com>
2025-08-02 10:12:36 +0200

    created seprated func for docker login, build, push

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -14,17 +14,18 @@ class Docker implements Serializable {
         this.script = script
     }
     
-    /**
-     * Build and push Docker image
-     * @param imageName Name of the Docker image to build
-     * @return void
-     */
-    def buildImage(String imageName) {
-        script.echo "building the docker image..."
+   
+    def dockerLogin() {
         script.withCredentials([script.usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t ${imageName} ."
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin 192.168.64.8:8083"
-            script.sh "docker push ${imageName}"
         }
+    }
+
+    def dockerBuild(String imageName) {
+        script.sh "docker build -t ${imageName} ."
+    }
+
+    def dockerPush(String imageName) {
+        script.sh "docker push ${imageName}"
     }
 }
