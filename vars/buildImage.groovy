38eecb9899a37acd6446f7c879c948fe248590b6
rajmandal800 <rajmandal800@gmail.com>
2025-08-02 09:29:52 +0200

    added Docker class

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,7 @@
-def call(String imageName) {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        // sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        // sh "echo $PASS | docker login -u $USER --password-stdin"
-        // sh 'docker push nanajanashia/demo-app:jma-2.0'
+#! usr/bin/env groovy
+import com.example.Docker
 
-        sh "docker build -t ${imageName} ."
-        sh "echo $PASS | docker login -u $USER --password-stdin 192.168.64.8:8083"
-        sh "docker push ${imageName}"
-    }
+def call(String imageName) {
+
+    return new Docker(this).buildImage(imageName)
 } 
